bb1c6ae8998902fa97e10fed16e7bc4922d247e2
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-01-15 07:26:58 +0900

    setState 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,70 +1,50 @@
+
 import React from 'react'
-import propTypes from 'prop-types'
+// import propTypes from 'prop-types'
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 4
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.3
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.5
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.8
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 3.4
+
+// function App() {
+//   return (
+//     <div className="App" >
+//       {
+//         foodILike.map((dish) => {
+//           return <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />
+//         })
+//       }
+//     </div>
+//   )
+// }
+class App extends React.Component {
+
+
+  add = () => {
+    this.setState(currunt => ({
+      age: currunt.age + 1
+    }));
   }
-];
 
-const Food = ({ name, picture, rating }) => {
-  return (
-    <>
-      <h2>I like {name}</h2>
-      <h5>{rating} / 5.0</h5>
-      <img src={picture} alt={name + " image"} />
-    </>
-  )
+  minus = () => {
+    this.setState({
+      age: this.state.age - 1
+    })
+  }
+
+  state = {
+    name: 'lightix',
+    age: 29
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Hello, I'm {this.state.name}</h1>
+        <h2>My age is...{this.state.age}</h2>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </>
+    )
+  }
 }
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div className="App" >
-      {
-        foodILike.map((dish) => {
-          return <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />
-        })
-      }
-    </div>
-  )
-}
 
 export default App;
