3476f6274a9b525e3693946d1eb9ad8e59c1bd53
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-01-15 08:50:40 +0900

    Life Cycle 학습

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-
 import React from 'react'
-// import propTypes from 'prop-types'
 
-
-// function App() {
-//   return (
-//     <div className="App" >
-//       {
-//         foodILike.map((dish) => {
-//           return <Food name={dish.name} picture={dish.image} key={dish.id} rating={dish.rating} />
-//         })
-//       }
-//     </div>
-//   )
-// }
 class App extends React.Component {
 
-
-  add = () => {
-    this.setState(currunt => ({
-      age: currunt.age + 1
-    }));
-  }
-
-  minus = () => {
-    this.setState({
-      age: this.state.age - 1
-    })
-  }
-
   state = {
-    name: 'lightix',
-    age: 29
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 3000)
   }
 
   render() {
+    const { isLoading } = this.state
     return (
       <>
-        <h1>Hello, I'm {this.state.name}</h1>
-        <h2>My age is...{this.state.age}</h2>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        <h1>{isLoading ? "Loading..." : "I'm ready!"}</h1>
       </>
     )
   }
 }
-
 
 export default App;
