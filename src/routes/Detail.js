9f056fae8821bbb5d345eb7a906cb11a860665ef
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-01-23 02:22:45 +0900

    Modifying CSS

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Detail.css'
 
 class Detail extends React.Component {
   conponentDidMount() {
@@ -7,11 +8,29 @@ class Detail extends React.Component {
       history.push('/')
     }
   }
+
   render(props) {
     const { location } = this.props
 
     if (location.state) {
-      return <h1>{location.state.title}</h1>
+      return (
+        <div className="detail__container">
+          <img src={location.state.poster} alt={location.state.title} />
+          <div className="detail__data">
+            <h3 className="detail__title">{location.state.title}</h3>
+            <div className="detail__info">
+              <h5 className="detail__year">{location.state.year}</h5>
+              <ul className="detail__genres">
+                {location.state.genres.map((genre, index) => {
+                  return <li className="detail__genre" key={index}>{genre}</li>
+                })}
+              </ul>
+            </div>
+            <p className="detail__summary">{location.state.summary.slice(0, 300)}...</p>
+          </div>
+
+        </div>
+      )
     } else {
       return null;
     }
